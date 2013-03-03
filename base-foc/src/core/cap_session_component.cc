941420b54451bc22be03d48eb2465a3f77dce3c7
Norman Feske <norman.feske@genode-labs.com>
2013-03-03 12:39:03 +0100

    foc: Remove dead code

--- base-foc/src/core/cap_session_component.cc
+++ base-foc/src/core/cap_session_component.cc
@@ -216,10 +216,3 @@ void Genode::Capability_map::remove(Genode::Cap_index* i)
 		cap_idx_alloc()->free(i, 1);
 	}
 }
-
-
-Genode::Cap_index_allocator* cap_idx_alloc()
-{
-	static Genode::Cap_index_allocator_tpl<Core_cap_index, 20*1024> _alloc;
-	return &_alloc;
-}
